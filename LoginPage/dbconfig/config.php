dd28f0938e2b97ddc4a46a9bd19a59c405b030a1
VijayNayakG <31723335+VijayNayakG@users.noreply.github.com>
2017-12-08 01:21:56 +0530

    Add files via upload

---
+++ LoginPage/dbconfig/config.php
@@ -0,0 +1,10 @@
+<?php
+/*For My LocalPC*/
+$con=mysqli_connect ("localhost", "root", "") or die ('I cannot connect to the database because: ' . mysql_error());
+mysqli_select_db ($con,'vjecompany');
+
+/*In place of 'vjecompany', enter your databasa name*/
+?>
+
+
+
